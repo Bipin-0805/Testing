0af39e27359cba448f1afb0fd80e6d34a82fcd31
Bipin-0805 <test.axel.01@gmail.com>
2023-12-12 18:37:12 +0530

    commit from USA 1st

---
+++ gitPractise/src/add/Addition.java
@@ -0,0 +1,18 @@
+package add;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a,b;
+		a=2;
+		b=3;
+				
+			int c= a+b;
+			System.out.println(c);
+		
+		
+	}
+
+}
